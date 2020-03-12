2e1503f8ccdda94484f82e69c5b5389e63d909df
JW Nicholson <john.nicholson812@gmail.com>
2020-03-12 16:21:49 -0400

    Error - no form control associated with firstnmae label. Check for use of "for" attribute

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 test("renders correctly", () => {
     render(<ContactForm />);
+});
+
+
+test("make sure form inputs are working", () => {
+    const {getByLabelText} = render(<ContactForm/>);
+
+    const firstName = getByLabelText(/first name/i)
+    const lastName = getByLabelText(/last name/i)
+    const email = getByLabelText(/email/i)
+    const message = getByLabelText(/message/i)
 });
