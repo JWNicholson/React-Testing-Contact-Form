8807d2547c249e563422c9f038478190b91fe7e6
JW Nicholson <john.nicholson812@gmail.com>
2020-03-12 16:42:42 -0400

    remove jest vsc plugin and run again

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -7,11 +7,28 @@ test("renders correctly", () => {
 });
 
 
-test("make sure form inputs are working", () => {
-    const {getByLabelText} = render(<ContactForm/>);
+test("make sure form inputs and submit button are working", () => {
+    const {getByLabelText, getByText, findByText} = render(<ContactForm/>);
 
-    const firstName = getByLabelText(/first name/i)
-    const lastName = getByLabelText(/last name/i)
-    const email = getByLabelText(/email/i)
-    const message = getByLabelText(/message/i)
+    const firstName = getByLabelText(/first name/i);
+    const lastName = getByLabelText(/last name/i);
+    const email = getByLabelText(/email/i);
+    const message = getByLabelText(/message/i);
+
+    fireEvent.change(firstName, {target:{name: 'firstName', value: 'Harry'}});
+    fireEvent.change(lastName, {target: {name: 'lastName', value: 'Norseman'}});
+    fireEvent.change(email, {target: {name: 'email', value: 'pillageandburn@thorshammer.no'}});
+    fireEvent.change(message, {targert: {name: 'message', value: 'Hello England'}});
+
+    const button = getByText(/submit/i);
+
+    //button clicked
+    fireEvent.click(button);
+
+    //assertions
+    findByText(/harry/i);
+    findByText(/Norseman/i);
+    findByText(/pillageandburn@thorshammer.no/i);
+    findByText(/hello england/i);
+
 });
