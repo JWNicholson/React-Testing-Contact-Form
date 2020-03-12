26ee0cd40d84289ab04bcc4f2bd3ca59c8840531
JW Nicholson <john.nicholson812@gmail.com>
2020-03-12 17:39:48 -0400

    started over with testing. Plan to try one input at a time and use getByTestID on form . firstName getByLableTetxt, fireEvent, and findByText working

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -7,28 +7,34 @@ test("renders correctly", () => {
 });
 
 
-test("make sure form inputs and submit button are working", () => {
-    const {getByLabelText, getByText, findByText} = render(<ContactForm/>);
+// test("make sure form inputs and submit button are working", () => {
+//     const { getByText, findByText, getByPlaceholderText} = render(<ContactForm/>);
+test("check firstName input and change", () => {
+    const {getByLabelText,findByText} = render(<ContactForm />);
+    const firstName = getByLabelText(/first name/i)
+    fireEvent.change(firstName, {target: {name: 'firstName', value: 'Holy'}})
+    findByText(/holy/i)
+})
 
-    const firstName = getByLabelText(/first name/i);
-    const lastName = getByLabelText(/last name/i);
-    const email = getByLabelText(/email/i);
-    const message = getByLabelText(/message/i);
+    // const firstName = getByPlaceholderText(/bill/i);
+    // const lastName = getByText(/last name/i);
+    // const email = getByText(/email/i);
+    // const message = getByText(/message/i);
 
-    fireEvent.change(firstName, {target:{name: 'firstName', value: 'Harry'}});
-    fireEvent.change(lastName, {target: {name: 'lastName', value: 'Norseman'}});
-    fireEvent.change(email, {target: {name: 'email', value: 'pillageandburn@thorshammer.no'}});
-    fireEvent.change(message, {targert: {name: 'message', value: 'Hello England'}});
+    // fireEvent.change(firstName, {target:{name: 'firstName', value: 'Harry'}});
+    // fireEvent.change(lastName, {target: {name: 'lastName', value: 'Norseman'}});
+    // fireEvent.change(email, {target: {name: 'email', value: 'pillageandburn@thorshammer.no'}});
+    // fireEvent.change(message, {targert: {name: 'message', value: 'Hello England'}});
 
-    const button = getByText(/submit/i);
+    // const button = getByText(/submit/i);
 
-    //button clicked
-    fireEvent.click(button);
+    // //button clicked
+    // fireEvent.click(button);
 
-    //assertions
-    findByText(/harry/i);
-    findByText(/Norseman/i);
-    findByText(/pillageandburn@thorshammer.no/i);
-    findByText(/hello england/i);
+    // //assertions
+    // findByText(/harry/i);
+    // findByText(/Norseman/i);
+    // findByText(/pillageandburn@thorshammer.no/i);
+    // findByText(/hello england/i);
 
-});
+// });
