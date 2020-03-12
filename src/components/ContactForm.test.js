3cb5d569a99e6ac06dc5e715340e61e0f2367b83
JW Nicholson <john.nicholson812@gmail.com>
2020-03-12 18:00:24 -0400

    input tests working. ready to test submit button

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -9,12 +9,28 @@ test("renders correctly", () => {
 
 // test("make sure form inputs and submit button are working", () => {
 //     const { getByText, findByText, getByPlaceholderText} = render(<ContactForm/>);
-test("check firstName input and change", () => {
+test("check firstName, lastName, message input and change", () => {
     const {getByLabelText,findByText} = render(<ContactForm />);
+    
     const firstName = getByLabelText(/first name/i)
-    fireEvent.change(firstName, {target: {name: 'firstName', value: 'Holy'}})
+    const lastName = getByLabelText(/last name/i)
+    const email = getByLabelText(/email/i)
+    const message = getByLabelText(/message/i)
+    
+
+    fireEvent.change(firstName, {target: {name: 'firstName', value: 'Ismell'}})
     findByText(/holy/i)
-})
+
+    fireEvent.change(lastName, {target: {name: 'lastName', value: 'Smoke'}})
+    findByText(/smoke/i)
+
+    fireEvent.change(email, {target: {name: 'email', value: 'malfunction@junction.com'}})
+    findByText(/email/i)
+
+    fireEvent.change(message, {target: {name: 'message', value: 'you must be burning me behind my back'}})
+    findByText(/message/i)
+
+});
 
     // const firstName = getByPlaceholderText(/bill/i);
     // const lastName = getByText(/last name/i);
